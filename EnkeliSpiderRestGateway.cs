43f03dd1fa17f0f2b65deba94e58d1aab1841a9b
Fonso <fonsopp@gmail.com>
2017-05-06 18:11:28 +0200

    First commit

---
+++ EnkeliSpiderRestGateway.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace RestClient
+{
+    public class EnkeliSpiderGateway
+    {
+        private const string SECURITY_TOKEN_HEADER = "es-token";
+        private const string START_EVENT_ENDPOINT = "api/v2/event/start";
+        private const string STOP_EVENT_ENDPOINT = "api/v2/event/stop";
+        private const string GET_RESULTS_ENDPOINT = "api/v2/event/{0}/results";
+        private const string DOWNLOAD_RESULT_ENDPOINT = "api/v2/event/{0}/result?name={1}";
+        private const string RESULTS_DIRECTORY = @"C:\test\";
+
+        private HttpClient client = new HttpClient();
+
+        public EnkeliSpiderGateway(string address, string token)
+        {
+            client.BaseAddress = new Uri(address);
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Add(SECURITY_TOKEN_HEADER, token);
+        }
+
+        public async Task<Event> StartEventAsync()
+        {
+            var response = await client.PostAsync(START_EVENT_ENDPOINT, null);
+            response.EnsureSuccessStatusCode();
+            var newEvent = await response.Content.ReadAsAsync<Event>();
+            return newEvent;
+        }
+
+        public async Task<Event> StopEventAsync()
+        {
+            var response = await client.PostAsync(STOP_EVENT_ENDPOINT, null);
+            response.EnsureSuccessStatusCode();
+            var newEvent = await response.Content.ReadAsAsync<Event>();
+            return newEvent;
+        }
+
+        public async Task<List<string>> RetrieveResultsAsync(int eventId)
+        {
+            var response = await client.GetAsync(string.Format(GET_RESULTS_ENDPOINT, eventId));
+            response.EnsureSuccessStatusCode();
+            var results = await response.Content.ReadAsAsync<List<string>>();
+            return results;
+        }
+
+        public async Task<bool> DownloadResultAsync(int eventId, string name)
+        {
+            var response = await client.GetAsync(string.Format(DOWNLOAD_RESULT_ENDPOINT, eventId, name));
+            response.EnsureSuccessStatusCode();
+            var result = await response.Content.ReadAsStreamAsync();
+            using (var fileStream = File.Create(RESULTS_DIRECTORY + name))
+            {
+                result.Seek(0, SeekOrigin.Begin);
+                result.CopyTo(fileStream);
+            }
+            return true;
+        }
+
+        public class Event
+        {
+            public int Id { get; set; }
+            public string Status { get; set; }
+
+            public string GetInfo()
+            {
+                return $"Event {Id}: {Status}";
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace RestClient
+{
+    class Program
+    {
+        private const string ADDRESS = "http://localhost:9090/";
+        private const string SECURITY_TOKEN = "in3a0497rg2ncak1ml6ttc6d2n";
+
+        static void Main()
+        {
+            RunAsync().Wait();
+        }
+
+        static async Task RunAsync()
+        {
+            Console.WriteLine("Push enter to start an event");
+            Console.ReadLine();
+            try
+            {
+                var client = new EnkeliSpiderGateway(ADDRESS, SECURITY_TOKEN);
+                var currentEvent = await client.StartEventAsync();
+                Console.WriteLine(currentEvent.GetInfo());
+                Console.WriteLine("Push enter to finish the event");
+                Console.ReadLine();
+                currentEvent = await client.StopEventAsync();
+                Console.WriteLine(currentEvent.GetInfo());
+                Console.WriteLine($"Results of event {currentEvent.Id}:");
+                var results = await client.RetrieveResultsAsync(currentEvent.Id);
+                results.ForEach(Console.WriteLine);
+                foreach (var result in results)
+                {
+                    await client.DownloadResultAsync(currentEvent.Id, result);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            Console.WriteLine("Push enter to exit");
+            Console.ReadLine();
+        }
+    }
+}
